01f271c42fe15b77c621d57bb222c14d4a9e4815
alstonjohnson <johnson.alston@icloud.com>
2024-06-27 17:30:14 -0500

    updated form.js to reflect correct function naming conventions

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,9 @@
 function validateForm() {
-    const name = document.getElementById('exampleFormControlInput1').value;
-    const email = document.getElementById('exampleFormControlInput2').value;
-    const password = document.getElementById('blog-content').value;
+    const usrnm = document.getElementById('exampleFormControlInput1').value;
+    const title = document.getElementById('exampleFormControlInput2').value;
+    const blogEntry = document.getElementById('blog-content').value;
 
-    if (name === '' || email === '' || password === '') {
+    if (usrnm === '' || title === '' || blogEntry === '') {
         alert('Please be sure to enter data into all fields before submitting this form.');
         return false; 
     }
@@ -12,16 +12,13 @@ function validateForm() {
 // Local Storage
 const form = document.querySelector('form');
 
-// Function for collecting the form data
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Collecting the form data
     const username = document.getElementById('exampleFormControlInput1').value;
     const blogTitle = document.getElementById('exampleFormControlInput2').value;
     const blogContent = document.getElementById('blog-content').value;
 
-    // Object for the collected form data
     const formDataObject = {
         username: username,
         blogTitle: blogTitle,
